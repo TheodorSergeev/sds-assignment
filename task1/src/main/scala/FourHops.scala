7d542a5d82cadf889851fb5cdf3a9e707d2e2140
Fedor Sergeev <theodor.sergeev@yandex.ru>
2022-04-06 12:21:21 +0200

    implement task1 FourHops

--- task1/src/main/scala/FourHops.scala
+++ task1/src/main/scala/FourHops.scala
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// <YOUR_FULL_NAME_HERE>
+// Fedor Sergeev
 //------------------------------------------------------------------------------
 
 import org.apache.log4j.Logger
@@ -25,9 +25,10 @@ object FourHops {
         // Count total vertices in the graph
         val totalVertices = countVertices(graph)
         println("Total Vertices", totalVertices)
-        
+
         // Find two hop neighbors
         val two_hop = matrixMultiply(graph, totalVertices)
+        //two_hop.collect().foreach(println)
         
         // Find four hop neighbors
         val quads = matrixMultiply(two_hop, totalVertices)
@@ -45,7 +46,14 @@ object FourHops {
     This function counts the number of distinct nodes in the input graph. Since each node 
     has atleast degree 1, it should appear in some pair (v1, v2).
     */
-    def countVertices(graph: GRAPH): Int =  ???
+    def countVertices(graph: GRAPH): Int = {
+        val vertices = graph
+            .flatMap(edge => Seq[Int](edge._1, edge._2))
+            .distinct()
+            .count().toInt
+
+        return vertices
+    }
 //------------------------------------------------------------------------------
 //  MapReduce
 //------------------------------------------------------------------------------
@@ -60,7 +68,20 @@ object FourHops {
     
     Identifying index helps multiply correct elements of A and B.
     */
-    def mmMapper(N: Int, index: (Int, Int)): List[((Int, Int), (Int, Int))] = ???
+    def mmMapper(N: Int, index: (Int, Int)): List[((Int, Int), (Int, Int))] = {
+        val A_id = 1
+        val B_id = 0
+
+        val i = index._1
+        val j = index._2
+
+        val k_list = (0 to N - 1).toList
+
+        val listA = k_list.map(k => ((i,k), (A_id, j)))
+        val listB = k_list.map(k => ((k,j), (B_id, i)))
+
+        return listA ++ listB
+    }
 
 //------------------------------------------------------------------------------
 
@@ -72,7 +93,22 @@ object FourHops {
     It must now produce the matrix product value (i, j, value). Set the value to 1 if it is 
     greater than 0 else let it be 0. Refer to test3 and test4 for an example case.
     */
-    def mmReducer(productElements: ((Int, Int), Iterable[(Int, Int)])): (Int, Int, Int) = ???
+    def mmReducer(productElements: ((Int, Int), Iterable[(Int, Int)])): (Int, Int, Int) = {
+        val A_id = 1
+        val B_id = 0
+
+        val mel = 0
+        val i = productElements._1._1
+        val j = productElements._1._2
+        val matr_els = productElements._2.groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
+        
+        // todo: can be optimized
+        val A_vec = matr_els(A_id).toSet
+        val B_vec = matr_els(B_id).toSet
+        val inter = A_vec.intersect(B_vec)
+
+        return (i, j, if (inter.isEmpty) 0 else 1)
+    }
 //------------------------------------------------------------------------------    
     
     /*
@@ -87,7 +123,16 @@ object FourHops {
 
     Note that this is sufficient for our goal of identifying all 4-hop neighbors of a node. 
     */
-    def matrixMultiply(matrix: GRAPH, N: Int): GRAPH = ???
+    def matrixMultiply(matrix: GRAPH, N: Int): GRAPH = {
+        val mult_matrix = matrix
+            .flatMap(edge => mmMapper(N, edge))
+            .groupByKey()
+            .map(productElements => mmReducer(productElements))
+            .filter(matrix_el => matrix_el._3 > 0)
+            .map(matrix_el => (matrix_el._1, matrix_el._2))
+
+        return mult_matrix
+    }
 
 //------------------------------------------------------------------------------
 //  Auxiliary functions
