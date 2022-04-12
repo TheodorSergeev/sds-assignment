0068c928c01a197541a28578a594c8f067f031eb
Fedor Sergeev <theodor.sergeev@yandex.ru>
2022-04-12 19:15:07 +0200

    add comments for the implementation

--- task1/src/main/scala/FourHops.scala
+++ task1/src/main/scala/FourHops.scala
@@ -47,12 +47,16 @@ object FourHops {
     has atleast degree 1, it should appear in some pair (v1, v2).
     */
     def countVertices(graph: GRAPH): Int = {
-        val vertices = graph
+        val n_vert = graph
+            // convert set of edges to set of all vertices with replicates
+            // e.g. {(i, j)} -> {i, j, ...}
             .flatMap(edge => Seq[Int](edge._1, edge._2))
+            // remove replicates
             .distinct()
+            // calculate the number of vertices and explicitly convert to int
             .count().toInt
 
-        return vertices
+        return n_vert
     }
 //------------------------------------------------------------------------------
 //  MapReduce
@@ -69,17 +73,24 @@ object FourHops {
     Identifying index helps multiply correct elements of A and B.
     */
     def mmMapper(N: Int, index: (Int, Int)): List[((Int, Int), (Int, Int))] = {
+        // identifiers for matrices A and B
         val A_id = 1
         val B_id = 0
 
+        // unpack edge into vertices i and j
         val i = index._1
         val j = index._2
 
+        // create an iterate for all matrix positions (0 to total number of vertices - 1)
         val k_list = (0 to N - 1).toList
 
+        // create maps for multiplication
+        // computation of P[i,k] requires A[i,j]
+        // computation of P[k,j] requires B[i,j]
         val listA = k_list.map(k => ((i,k), (A_id, j)))
         val listB = k_list.map(k => ((k,j), (B_id, i)))
 
+        // concatenate lists (to be passed to the reducer)
         return listA ++ listB
     }
 
@@ -94,19 +105,30 @@ object FourHops {
     greater than 0 else let it be 0. Refer to test3 and test4 for an example case.
     */
     def mmReducer(productElements: ((Int, Int), Iterable[(Int, Int)])): (Int, Int, Int) = {
+        // identifiers for matrices A and B
         val A_id = 1
         val B_id = 0
 
-        val mel = 0
+        // unpack productElements into the vertices i and j
         val i = productElements._1._1
         val j = productElements._1._2
-        val matr_els = productElements._2.groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
-        
-        // todo: can be optimized
+
+        val matr_els = productElements._2
+            // group by matrix identifier (1 for A, 0 for B)
+            .groupBy(_._1)
+            // here map structure is {matrix id -> map{matrix id -> vertice}}
+            // we convert it to {matrix id -> list of vertices}
+            .map { case (k, v) => (k, v.map(_._2))}
+
+        // extract non-zero elements for A and B
         val A_vec = matr_els(A_id).toSet
         val B_vec = matr_els(B_id).toSet
-        val inter = A_vec.intersect(B_vec)
 
+        // intersection is the non-zero elements of P = A.B
+        val inter = A_vec.intersect(B_vec)
+        // todo: set intersection is slow => this procedure can be optimized?
+
+        // return edge and non-edge (i,j) (non-edges are filtered out in the multiplication)
         return (i, j, if (inter.isEmpty) 0 else 1)
     }
 //------------------------------------------------------------------------------    
@@ -125,10 +147,15 @@ object FourHops {
     */
     def matrixMultiply(matrix: GRAPH, N: Int): GRAPH = {
         val mult_matrix = matrix
+            // perform map to obtain ((i, j), (identifying_matrix, identifying_index))
             .flatMap(edge => mmMapper(N, edge))
+            // group by edges
             .groupByKey()
+            // perform reduce to obtain {(i, j, 1 if edge is present or 0 otherwise)}
             .map(productElements => mmReducer(productElements))
+            // get rid of zero elements of the product matrix aka (i,j,0)
             .filter(matrix_el => matrix_el._3 > 0)
+            // since 0s are filtered, drop the "1" referring to non-zero elements: (i,j,1) -> (i,j) 
             .map(matrix_el => (matrix_el._1, matrix_el._2))
 
         return mult_matrix
